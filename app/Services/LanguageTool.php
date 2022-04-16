bf06aa5fbae754fd8b779bec029acdb568d4d26c
Hemn Movafeghi <hemn791@gmail.com>
2022-04-16 17:17:46 +0300

    remove matches property

--- app/Services/LanguageTool.php
+++ app/Services/LanguageTool.php
@@ -9,14 +9,13 @@ class LanguageTool implements SpellChecker
 {
     const URL = "https://languagetool.org/api/";
     private string $message, $language, $corrected;
-    private array $matches;
 
     public function fix(string $message, string $language): string
     {
         $this->message = $message;
         $this->language = $language;
-        $this->getMisspellMatches();
-        $this->applyReplacements();
+        $matches = $this->getMisspellMatches();
+        $this->applyReplacements($matches);
         return $this->corrected;
     }
 
@@ -24,13 +23,13 @@ class LanguageTool implements SpellChecker
     {
         $client = new Client();
         $resp = $client->request("get", self::URL . "v2/check?text={$this->message}&language={$this->language}");
-        $this->matches = json_decode($resp->getBody()->getContents())->matches;
+        return json_decode($resp->getBody()->getContents())->matches;
     }
 
-    private function applyReplacements()
+    private function applyReplacements($matches)
     {
         $corrected = $this->message;
-        foreach ($this->matches as $match) {
+        foreach ($matches as $match) {
             $misspell = substr($this->message, $match->offset, $match->length);
             $corrected = str_replace($misspell, $match->replacements[0]->value, $corrected);
         }
