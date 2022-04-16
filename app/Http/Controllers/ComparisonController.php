747834d0d8d6fa78bf4305b0fb2b86fff63a808e
Hemn Movafeghi <hemn791@gmail.com>
2022-04-16 22:38:45 +0300

    add ComparisonRequest

--- app/Http/Controllers/ComparisonController.php
+++ app/Http/Controllers/ComparisonController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ComparisonRequest;
 use App\Services\ApiResponse;
 use App\Services\Comparison;
 use Illuminate\Http\Request;
@@ -10,7 +11,7 @@ use Illuminate\Http\Response;
 class ComparisonController extends Controller
 {
 
-    public function getDifferences(Request $request): Response
+    public function getDifferences(ComparisonRequest $request): Response
     {
         return ApiResponse::successResponse("comparison", Comparison::getDifferences($request->error_messages));
     }
---
+++ app/Http/Requests/ComparisonRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ComparisonRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            "error_messages" => "required|array|min:1",
+            "error_messages.*.title" => "required",
+            "error_messages.*.module" => "required",
+            "error_messages.*.language"=> "required",
+            "error_messages.*.language.code"=> "required",
+            "error_messages.*.message" => "required",
+            "error_messages.*.original_message" => "required",
+        ];
+    }
+}
